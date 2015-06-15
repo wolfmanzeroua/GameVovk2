0ff4d99f66f9a5c6cd29945e34f6c2acb6d3670f
wolfmanzeroua <wolfmanzeroua@gmail.com>
2015-06-16 02:30:06 +0300

    small bugs

--- public/js/views/Hero/changeView.js
+++ public/js/views/Hero/changeView.js
@@ -28,7 +28,46 @@ define([
         },
 
         heroSelected: function() {
-            console.log('heroSelected');
+            console.log('Selected heroID:',  $('#heroSelect').val());
+            var hero= this.collection.at( $('#heroSelect').val()).toJSON();
+
+
+            $('#name').val( hero.name);
+            hero.clan=='X-Men' ? $('#clan')[0].checked = true : $('#clan2')[0].checked = true;
+            $('#features').val(hero.features);
+            $('#hairСolor').val(hero.hairСolor);
+            $('#beard').val(hero.beard);
+            $('#tits').val(hero.tits);
+            hero.sex == 'Man'? $('#sex')[0].checked  = true : $('#sex2')[0].checked  = true;
+            $('#health').val(hero.health);
+            $('#maxHealth').val(hero.maxHealth);
+            $('#power').val(hero.power);
+            $('#attackRange').val(hero.attackRange);
+            $('#damage').val(hero.damage);
+            $('#specDamage').val(hero.specDamage);
+            $('#chanceSpecDamage').val(hero.chanceSpecDamage);
+            //hero.defence = $('#defence').val();
+            $('#speed').val(hero.speed);
+            hero.canFly ? $('#canFly')[0].checked = true: $('#canFly2')[0].checked = true;
+            $('#flySpeed').val(hero.flySpeed);
+            hero.canBeInvisible ? $('#canBeInvisible')[0].checked = true: $('#canBeInvisible2')[0].checked = true ;
+            hero.invisible ? $('#invisible')[0].checked = true : $('#invisible2')[0].checked = true;
+            hero.canJump ? $('#canJump')[0].checked = true : $('#canJump2')[0].checked = true;
+            hero.canTeleport ? $('#canTeleport')[0].checked= true :$('#canTeleport2')[0].checked ;
+            hero.canShoot ? $('#canShoot')[0].checked = true : $('#canShoot2')[0].checked = true;
+            hero.canFreeze ? $('#canFreeze')[0].checked = true : $('#canFreeze2')[0].checked = true;
+            hero.isFreeze ? $('#isFreeze')[0].checked = true : $('#isFreeze2')[0].checked = true;
+            hero.isFly ? $('#isFly')[0].checked = true : $('#isFly2')[0].checked = true;
+            hero.canHealing ? $('#canHealing')[0].checked= true : $('#canHealing2')[0].checked= true;
+            $('#healingMaxPoint').val(hero.healingMaxPoint);
+            hero.hasVampBite ? $('#hasVampBite')[0].checked = true :$('#hasVampBite2')[0].checked= true;
+            hero.lookForTrouble ? $('#lookForTrouble')[0].checked = true : $('#lookForTrouble2')[0].checked = true;
+            $('#x').val(hero.x);
+            $('#y').val(hero.y);
+            $('#nextDestinationPointX').val(hero.nextDestinationPointX);
+            $('#nextDestinationPointY').val(hero.nextDestinationPointY);
+
+
         },
 
 
@@ -77,6 +116,7 @@ define([
                         .attr("value",item.attributes.heroID)
                         .text(item.attributes.name));
             });
+
             //this.collection.forEach( function(item){
             //               var hero =  item;
             //               console.log('hero',hero);
